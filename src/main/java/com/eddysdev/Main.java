9acce0e98eea3b1926c6431466cdc82d03920613
Eddys Gonzalez <eddysgonzalezdev@gmail.com>
2026-07-02 00:15:55 -0500

    Replacement of MAIN, Add HTTP client, Gson deserialization and List<User> support

--- src/main/java/com/eddysdev/Main.java
+++ src/main/java/com/eddysdev/Main.java
@@ -1,40 +1,71 @@
 package com.eddysdev;
 
 import com.google.gson.Gson;
+import com.eddysdev.model.*;
 
-class Usuario {
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
+import java.util.List;
 
-    String nombre;
-    int edad;
-
-    Usuario(String nombre, int edad) {
-        this.nombre = nombre;
-        this.edad = edad;
-    }
-}
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class Main {
 
     public static void main(String[] args) {
+        HttpClient client = HttpClient.newHttpClient();
 
-        Usuario usuario = new Usuario("Eddye", 25);
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://jsonplaceholder.typicode.com/users"))
+                .build();
+
+        HttpResponse<String> response;
+
+        try {
+            response =
+                    client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        String json = response.body();
+        //System.out.println(json);
 
         Gson gson = new Gson();
 
-        String json = gson.toJson(usuario);
+        /*
 
-        String metodo = """
-                {
-                    "nombre":"Eddys",
-                    "edad":22
-                }
-                """;
-        Usuario pruebametodo = gson.fromJson(metodo, Usuario.class);
+        User[] users = gson.fromJson(json, User[].class);
 
-        System.out.println(pruebametodo.nombre);
-        System.out.println(pruebametodo.edad);
-        System.out.println(metodo);
+        for (int i = 0; i < users.length; i++) {
+            System.out.println(
+                    "User name: "+ users[i].name
+                    + "\nEmail: " + users[i].email
+                    + '\n'
+            );
+        }
 
-        System.out.println(json);
+        */
+
+        Type type = new TypeToken<List<User>>() {}.getType();
+
+        List<User> users = gson.fromJson(json, type);
+
+        for (User user : users) {
+            System.out.printf("""
+                    User ID: %d
+                    Name: %s
+                    Email: %s
+                    City: %s
+                    Phone: %s%n
+                    """
+                    , user.id, user.name, user.email, user.address.city, user.phone
+            );
+        }
     }
 }
